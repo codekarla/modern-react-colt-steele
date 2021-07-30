e063adeb51db51c824c332da9af5a1a2d4ac6593
Karla Vargas <kleandrav@gmail.com>
2021-07-30 23:33:54 +0800

    Add the course section it came from

--- casino/pages/index.js
+++ casino/pages/index.js
@@ -1,66 +1,70 @@
-import Head from "next/head";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+import { Tooltip } from 'antd';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Casino!</title>
-        <meta
-          name="description"
-          content="Mini Exercises - from the Modern React Bootcamp by Colt Steele"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Casino!</title>
+                <meta
+                    name='description'
+                    content='Mini Exercises - from the Modern React Bootcamp by Colt Steele'
+                />
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to{" "}
-          <a
-            href="https://www.udemy.com/course/modern-react-bootcamp/"
-            target="blank"
-          >
-            Casino!
-          </a>
-        </h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Welcome to{' '}
+                    <a
+                        href='https://www.udemy.com/course/modern-react-bootcamp/'
+                        target='blank'
+                    >
+                        Casino!
+                    </a>
+                </h1>
 
-        <p className={styles.description}>
-          Choose a <code className={styles.code}>game</code> to play:
-        </p>
+                <p className={styles.description}>
+                    Choose a <code className={styles.code}>game</code> to play:
+                </p>
 
-        <div className={styles.grid}>
-          <Link href="/slotmachine" as="/meow-slot-machine">
-            <a className={styles.card}>
-              <h2>Meow Slot Machine &rarr;</h2>
-              <p>A dumb slot machine featuring meows.</p>
-            </a>
-          </Link>
+                <div className={styles.grid}>
+                    <Link href='/slotmachine' as='/meow-slot-machine'>
+                        <a className={styles.card}>
+                            <h2>Meow Slot Machine &rarr;</h2>
+                            <p>
+                                A dumb slot machine featuring meows.{' '}
+                                <small>(Section 3: Props & More)</small>
+                            </p>
+                        </a>
+                    </Link>
 
-          <Link href="/x">
-            <a className={styles.card}>
-              <h2>X &rarr;</h2>
-              <p>Xxxxx...</p>
-            </a>
-          </Link>
+                    <Link href='/x'>
+                        <a className={styles.card}>
+                            <h2>X &rarr;</h2>
+                            <p>Xxxxx...</p>
+                        </a>
+                    </Link>
 
-          <Link href="/y">
-            <a className={styles.card}>
-              <h2>Y &rarr;</h2>
-              <p>Yyyyy...</p>
-            </a>
-          </Link>
+                    <Link href='/y'>
+                        <a className={styles.card}>
+                            <h2>Y &rarr;</h2>
+                            <p>Yyyyy...</p>
+                        </a>
+                    </Link>
 
-          <Link href="/z">
-            <a className={styles.card}>
-              <h2>Z &rarr;</h2>
-              <p>Zzzzz...</p>
-            </a>
-          </Link>
+                    <Link href='/z'>
+                        <a className={styles.card}>
+                            <h2>Z &rarr;</h2>
+                            <p>Zzzzz...</p>
+                        </a>
+                    </Link>
+                </div>
+            </main>
+
+            <footer className={styles.footer}>July 2021</footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>July 2021</footer>
-    </div>
-  );
+    );
 }
